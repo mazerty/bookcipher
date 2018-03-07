0b178716b6f44f338577cc8b2865828cacff0779
mazerty <mazerty@gmail.com>
2018-03-07 08:47:07 +0100

    test suite

---
+++ test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -ex
+
+# cleans a potentially failed previous test run
+[ -d tmp/ ] && rm -rf tmp/
+
+# creates tmp directory
+mkdir tmp
+
+# the cipher being somewhat random, you can't unit-test it in a simple way
+# so first we're gonna unit-test the decipher, then test the whole cipher-decipher process to cover everything
+
+# initializes the reference file
+prfg test 1000000 > tmp/reference
+
+# tests the decipher
+../src/bookcipher medium tmp/reference results/cipher tmp/decipher
+diff results/original tmp/decipher
+
+# tests the cipher-decipher
+../src/bookcipher hard tmp/reference results/original tmp/cipher
+../src/bookcipher medium tmp/reference tmp/cipher tmp/decipher
+diff results/original tmp/decipher
+
+# cleans tmp directory
+rm -rf tmp
